5ad484f016d3c8431a6804f3d17dfdfe96ea32a2
Aynal1991 <housemilon23@gmail.com>
2025-08-24 12:11:13 +0600

    admin setup

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+admin.site.site_header = "Rice Vandar Admin"
+admin.site.site_title = "Rice Vandar Admin Portal"
+admin.site.index_title = "Welcome to Rice Vandar Admin"
+from .models import Cart, CartItem, Order, OrderItem
+admin.site.register(Cart)
+admin.site.register(CartItem)
+admin.site.register(Order)
+admin.site.register(OrderItem)
